ad2efc2c948446297b105bfcbc1183f03c329946
Daniel Rammer <dnl.rammer@gmail.com>
2016-11-30 18:01:53 +0100

    initial commit

---
+++ Assets/MoveOnLook.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Tobii.EyeTracking;
+
+public class MoveOnLook : MonoBehaviour {
+    GazeAware ga;
+	// Use this for initialization
+	void Start () {
+        ga = GetComponent<GazeAware>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (ga.HasGazeFocus) {
+            transform.Translate(0, 0, 1 * Time.deltaTime);
+        }
+	}
+}
