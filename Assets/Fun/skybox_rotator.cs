d25f46252b1d15e21a0935f59f36ed124420ae62
Zeladin <f.zolda@gmail.com>
2016-12-01 11:34:07 +0100

    Rotate skybox

---
+++ Assets/Fun/skybox_rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class skybox_rotator : MonoBehaviour {
+    private Material skybox;
+
+    // Use this for initialization
+    void Start () {
+        skybox = RenderSettings.skybox;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float asd = skybox.GetFloat("_Rotation");
+        skybox.SetFloat("_Rotation", asd + 0.1f);
+    }
+}
